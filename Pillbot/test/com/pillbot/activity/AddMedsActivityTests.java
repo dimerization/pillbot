cfe4dffc4567727483c68012a027c4db10539e6b
dimerization <maura.c.warner@gmail.com>
2016-09-27 18:28:54 -0400

    Test- initializing my dev branch

--- Pillbot/test/com/pillbot/activity/AddMedsActivityTests.java
+++ Pillbot/test/com/pillbot/activity/AddMedsActivityTests.java
@@ -1,7 +1,33 @@
 package com.pillbot.activity;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class AddMedsActivityTests extends TestCase {
-	// TODO get test coverage up to 100% for AddMedsActivity
+
+	@Test
+	public void testOnCreate() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testOnCreateOptionsMenu() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testOnOptionsItemSelected() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testSave() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testCancel() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
 }
--- Pillbot/test/com/pillbot/activity/MainActivityTests.java
+++ Pillbot/test/com/pillbot/activity/MainActivityTests.java
@@ -1,7 +1,14 @@
 package com.pillbot.activity;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class MainActivityTests extends TestCase {
+
+	@Test
+	public void testOnCreate() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
 	// TODO get test coverage up to 100% for MainActivity
 }
